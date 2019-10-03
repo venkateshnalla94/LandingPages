6a9451aec4b3ee3f3fc9f47dda6ffd2c95337d18
imvamsi <krrishvamsi7@gmail.com>
2019-10-02 21:37:56 -0400

    added contact us page :beer:

--- src/Pages/NavBarG.jsx
+++ src/Pages/NavBarG.jsx
@@ -8,11 +8,20 @@ import Button from "react-bootstrap/Button";
 const NavBarG = () => {
   return (
     <div style={{ width: "100%" }}>
-      <Navbar sticky="top" collapseOnSelect expand="lg" bg="dark width" variant="dark">
-        <Navbar.Brand href="#home"><img src="https://gurupalacerestaurant.com/wp-content/uploads/2019/04/logo-1.png"
-          alt="logo"
-          style={{ height: "auto", width: "60%" }}
-        ></img></Navbar.Brand>
+      <Navbar
+        sticky="top"
+        collapseOnSelect
+        expand="lg"
+        bg="dark width"
+        variant="dark"
+      >
+        <Navbar.Brand href="#home">
+          <img
+            src="https://gurupalacerestaurant.com/wp-content/uploads/2019/04/logo-1.png"
+            alt="logo"
+            style={{ height: "auto", width: "60%" }}
+          ></img>
+        </Navbar.Brand>
         <Navbar.Brand href="#home">Home</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
@@ -21,23 +30,25 @@ const NavBarG = () => {
             <Nav.Link href="#pricing">Gallery</Nav.Link>
             <NavDropdown title="Menu" id="collasible-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
             </NavDropdown>
             <Nav.Link href="#pricing">Banquet Facility</Nav.Link>
             <Nav.Link href="#pricing">Catering</Nav.Link>
-            <Nav.Link href="#pricing">Contact Us</Nav.Link>
+            <Nav.Link href="/contactus">Contact Us</Nav.Link>
           </Nav>
           <Nav>
             <Nav.Link href="#deets">Book A Banquet</Nav.Link>
-
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-
     </div>
-  )
-}
+  );
+};
 export default NavBarG;
